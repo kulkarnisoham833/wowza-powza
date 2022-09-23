c0ed2ded1a5432b9d1eb799af248d6d84981b8f5
Soham Kulkarni <63484051+kulkarnisoham833@users.noreply.github.com>
2022-09-23 01:10:30 +0000

    fds

--- main.sh
+++ main.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+
+
+#unattended upgrades
+dpkg-reconfigure -plow unattended-upgrades
+
+#bash upgrade
+apt install --only-upgrade bash
+#kernel upgrade
+apt-get dist-upgrade
+#remove unnecessary dependencies
+apt autoremove
+#remove unsupported pkgs
+apt autoclean
+
+#user management ---- https://askubuntu.com/questions/1291961/how-to-take-input-from-user-until-a-specific-character-appears
+
+read -d $ -p 'input characters: '
+[[ $? = 0 ]] && \
+printf 'Jackpot! You typed: %s\n' $
+
+
 
