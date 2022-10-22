89a24bfc3ea3ea264b994ccad4ab7bd45bfabb1c
lamentablem <113483148+lamentablem@users.noreply.github.com>
2022-10-21 20:37:02 -0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -2,10 +2,10 @@
 
 # https://github.com/k4yt3x/sshd_config
 chmod 640 /etc/ssh
-chmod 700 ~/.ssh
-chmod 644 ~/.ssh/rsa.pub
-chmod 600 ~/.ssh/id_rsa
-chmod 600 ~/.ssh/authorized_keys
+chmod 700 /.ssh
+chmod 644 /.ssh/rsa.pub
+chmod 600 /.ssh/id_rsa
+chmod 600 /.ssh/authorized_keys
 
 cat betterssh > /etc/ssh/sshd_config
 echo "SSH config file updated"
